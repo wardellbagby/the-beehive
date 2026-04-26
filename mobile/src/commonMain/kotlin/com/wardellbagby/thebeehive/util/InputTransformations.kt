3192e05cd258cacc37ebb874a75a93b5263c92d3
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2026-04-25 20:16:32 -0700

    Negative Zeros?
    - Fix the input transformation to be a little nicer when inputting
    numbers and stop allowing negatives since there's no feature that needs
    (or supports that...) that.

--- mobile/src/commonMain/kotlin/com/wardellbagby/thebeehive/util/InputTransformations.kt
+++ mobile/src/commonMain/kotlin/com/wardellbagby/thebeehive/util/InputTransformations.kt
@@ -3,13 +3,13 @@ package com.wardellbagby.thebeehive.util
 import androidx.compose.foundation.text.input.InputTransformation
 
 val INT_ONLY_INPUT_TRANSFORMATION = InputTransformation {
-  // todo this doesn't allow adding negative numbers or easily going from 0 to 8
-  if (length == 0) {
-    append("0")
-  }
-  val isNumeric = asCharSequence().toString().toIntOrNull() != null
-
-  if (!isNumeric) {
-    revertAllChanges()
+  val text = asCharSequence().toString()
+  when {
+    text.isEmpty() -> append("0")
+    text.toIntOrNull() == null -> revertAllChanges()
+    else -> {
+      val normalized = text.toInt().toString()
+      if (text != normalized) replace(0, length, normalized)
+    }
   }
 }
