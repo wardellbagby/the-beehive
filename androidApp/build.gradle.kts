63fbed410ffaf919f8d22ce8894a75225111ec0f
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2026-05-01 12:25:33 -0700

    chore: bump version to v0.0.3

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.wardellbagby.thebeehive"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = 3
-    versionName = "0.0.2"
+    versionCode = 4
+    versionName = "0.0.3"
   }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
   buildTypes { getByName("release") { isMinifyEnabled = false } }
