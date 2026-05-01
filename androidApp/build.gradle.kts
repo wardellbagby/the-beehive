9e8d61b48a8244a331a3220d37d4e090fddb944f
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2026-05-01 11:57:39 -0700

    chore: bump version to v0.0.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.wardellbagby.thebeehive"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "0.0.1"
   }
   packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
   buildTypes { getByName("release") { isMinifyEnabled = false } }
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.wardellbagby.thebeehive"
 
-version = "1.0.0"
+version = "0.0.1"
 
 application {
   mainClass.set("com.wardellbagby.thebeehive.ApplicationKt")
